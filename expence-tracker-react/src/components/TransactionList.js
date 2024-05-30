7017a87987a7a441b9f077fd255dd0de94cb0754
JoachimAndreBergan <jobe016@student.kristiania.no>
2024-05-31 00:02:24 +0200

    pulled readMe

--- expence-tracker-react/src/components/TransactionList.js
+++ expence-tracker-react/src/components/TransactionList.js
@@ -1,17 +1,16 @@
-import { useContext } from 'react'
+import React, { useContext } from 'react';
 import { Transaction } from './Transaction';
 
-import { GlobalContext } from '../context/GlobalState'
+import { GlobalContext } from '../context/GlobalState';
 
 export const TransactionList = () => {
   const { transactions } = useContext(GlobalContext);
 
   return (
     <>
-        <h3>History</h3>
-      <ul clasName="list">
-        {transactions.map(transaction =>(<Transaction key={transaction.id} transaction={transaction}/>))}
-        
+      <h3>History</h3>
+      <ul className="list">
+        {transactions.map(transaction => (<Transaction key={transaction.id} transaction={transaction} />))}
       </ul>
     </>
   )
